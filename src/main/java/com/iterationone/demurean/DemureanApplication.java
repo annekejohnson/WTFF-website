86080863ca8c06d1ecaa84d5bb2de455c10dccc7
demurean <arielle.felicia@gmail.com>
2024-03-08 15:05:26 -0800

    newpages

---
+++ src/main/java/com/iterationone/demurean/DemureanApplication.java
@@ -0,0 +1,13 @@
+package com.iterationone.demurean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemureanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemureanApplication.class, args);
+	}
+
+}
