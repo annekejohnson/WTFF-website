8f01bbeaf8a49a32688d0a079e533e537e35e068
caokhanhquynh <117809590+caokhanhquynh@users.noreply.github.com>
2024-03-22 16:41:53 -0700

    add, delete, update courses

---
+++ src/main/java/com/example/demo/models/CourseRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.models;
+
+// import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseRepository extends JpaRepository<Course, String>{
+    Course findByCoursename(String coursename);
+    void deleteByCoursename(String coursename);
+}
