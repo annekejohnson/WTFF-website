b7e0fa98753f5c54998e58ff0c91356979b192e9
fearyfiremast <fearyfiremast@gmail.com>
2024-06-01 23:18:52 -0700

    Added email string type

--- src/main/java/com/example/demo/models/User.java
+++ src/main/java/com/example/demo/models/User.java
@@ -7,40 +7,48 @@ import jakarta.persistence.*;
 public class User {
     @Id
     private String username;
+    private String email;
     private String password;
     private String usertype;
 
-    public User(String username, String password, String usertype) {
+    public User(String username, String password, String usertype, String Email) {
         this.username = username;
         this.password = password;
         this.usertype = usertype;
+        this.email = Email;
     }
 
     public User() { 
     }
 
+    // Username
     public String getUsername() {
         return username;
     }
-
     public void setUsername(String username) {
         this.username = username;
     }
 
+    // Password
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
 
+    // Usertype
     public String getUsertype() {
         return usertype;
     }
-
     public void setUsertype(String usertype) {
         this.usertype = usertype;
     }
+
+    // Email
+    public String getEmail() {
+        return this.email;
+    }
+    public void setEmail(String email) { this.email = email;}
    
 }
